0d7e0574c8c48198fc123a5e4790f245d23d6bbe
Vinayakkh <94226258+Vinayakkh@users.noreply.github.com>
2023-01-05 22:04:34 +0530

    Add files via upload

---
+++ pages/aboutauthor.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>About THe Developer</title>
+	<!-- Bootstrap Core CSS -->
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+
+<!-- MetisMenu CSS -->
+<link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+<!-- DataTables CSS -->
+ <link href="../css/dataTables/dataTables.bootstrap.css" rel="stylesheet">
+ 
+<!-- DataTables Responsive CSS -->
+<link href="../css/dataTables/dataTables.responsive.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+<link rel="stylesheet" href="../icofont/icofont.min.css">
+
+</head>
+
+
+<body>
+<div id="wrapper">
+
+<?php include 'includes/nav.php'?>
+
+
+<div id="page-wrapper">
+<div class="container-fluid">
+<div class="row">
+<div class=".col-lg-12">
+               <h1 class="page-header">About The Developers</h1>
+               <h3>This Is Aimed To Maintain The Record Of The Donors. This is develop with the help of php,css,jquery,bootstrap,html,MySql and css. Developed by TrixyWeb , Versha and Vinayak. </h3>
+                </div>
+  </div>  
+	 
+
+    
+</head>
+
+	<body>
+		<div id="wrapper">
+
+
+        
+	 
+<style>
+h2    {
+
+
+
+    text-align: center; 
+     
+}
+</style>
+</div>
+
+<!-- jQuery -->
+  <script src="../vendor/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="../vendor/metisMenu/metisMenu.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="../dist/js/sb-admin-2.js"></script>
+
+<!-- DataTables JavaScript -->
+<script src="../js/dataTables/jquery.dataTables.min.js"></script>
+<script src="../js/dataTables/dataTables.bootstrap.min.js"></script>
+
+
+</body>
+	
+<footer>
+        <p>&copy; <?php echo date("Y"); ?>: Developed By Vinayak Khandelwal</p>
+    </footer>
+    <style>
+	footer{
+   background-color: #424558;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 35px;
+    text-align: center;
+    color: #CCC;
+}
+footer p {
+    padding: 10.5px;
+    margin: 0px;
+    line-height: 100%;
+}
+	</style>
+</html>
